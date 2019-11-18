87917ed3d63fe465ab72216faa49f8dd03ce2fe6
Guillaume Jasmin <guillaume.jasmin@gmail.com>
2019-11-18 11:17:19 +0100

    fix: throw error and add message about axios@0.19 incompatibility

--- src/addAxiosInterceptor.ts
+++ src/addAxiosInterceptor.ts
@@ -4,6 +4,13 @@ export function addAxiosInterceptor(api: RSAxiosInstance<'', {}>, schema: Schema
   // @ts-ignore
   api.interceptors.request.use((config: AxiosRequestConfigBase) => {
     const { routeName, urlParams, ...restConfig } = config
+    if (!routeName) {
+      throw new Error(`
+        request-schema: routeName is undefined.
+        \nIf you use axios@0.19, downgrade to axios@0.18 because request-schema is incomptabible with v0.19
+        \nsee breaking changes issue with custom config https://github.com/axios/axios/issues/1718
+      `)
+    }
     const { url, method } = schema[routeName]
     const finalURL = typeof url === 'function'
       ? url(config.urlParams)
